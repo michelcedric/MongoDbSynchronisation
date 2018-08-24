d2ec5c8723c589410bd8be64d6e5f63b1aa5c263
Cedric <cedric.michel@outlook.be>
2018-08-24 19:26:30 +0200

    remove old files

--- Soroban.Infrastructure/IMongoCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-
-namespace Elia.Soroban.Infrastructure
-{
-    public interface IMongoDbCollection<TEntity>
-    {
-        IMongoCollection<TEntity> Collection { get; }
-    }
-}
--- Soroban.Infrastructure/Mapping/IMap.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Elia.Soroban.Infrastructure.Mapping
-{
-    /// <summary>
-    /// Definition of object that can be map
-    /// </summary>
-    public interface IMap
-    {
-    }
-}
