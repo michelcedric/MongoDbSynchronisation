5b847a3328245617b63fd8608e8fe46eec26ce60
Cedric <cedric.michel@outlook.be>
2018-08-26 17:37:32 +0200

    add extension and generation

---
+++ MongoSync/Vsts-Extension/home/buildAndReleaseTask/tool.ps1
@@ -0,0 +1,38 @@
+﻿[CmdletBinding()]
+param()
+Trace-VstsEnteringInvocation $MyInvocation
+
+try
+{
+	Write-Host "... Powershell Starting ..."
+
+	$parameter = Get-VstsInput -Name parameter -Require
+
+	Write-Host "Parameter : $($parameter)"
+
+	$exeFileName = "tool/MongoSync.exe"
+	$exeFilePath  = Join-Path $PSScriptRoot $exeFileName 
+
+	$pinfo = New-Object System.Diagnostics.ProcessStartInfo 
+	$pinfo.FileName = $exeFilePath
+	$pinfo.Arguments = $parameter
+	$pinfo.UseShellExecute = $false 
+	$pinfo.CreateNoWindow = $true 
+	$pinfo.RedirectStandardOutput = $true 
+	$pinfo.RedirectStandardError = $true
+
+	$process= New-Object System.Diagnostics.Process 
+	$process.StartInfo = $pinfo
+	$process.Start() | Out-Null 
+	$process.WaitForExit()
+
+
+	$stdout = $process.StandardOutput.ReadToEnd();
+
+	Write-Host $stdout 
+
+	Write-Host "... Powershell Step finished ..."
+
+}finally{
+	 Trace-VstsLeavingInvocation $MyInvocation
+}
---
+++ MongoSync/Vsts-Extension/home/package.bat
@@ -0,0 +1,2 @@
+echo create extension
+tfx extension create --manifest-globs vss-extension.json
